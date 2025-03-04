8cfb1f9ecf3fd1a79311d613bb7c45ada43e6dc0
Nguyen Toan <ngoctoan06011998>
2025-03-04 13:19:39 +0700

    update: ProductDetailPage

--- src/app/product/[id]/page.tsx
+++ src/app/product/[id]/page.tsx
@@ -1,19 +1,21 @@
 import { ProductService } from "@/services";
 import { unstable_cache } from "next/cache";
+import { PageProps } from "../../../../.next/types/app/page";
 
-const getDetail = (id:number|string) => unstable_cache(async()=>{
-  const response = await ProductService.show(id)
-  return response.context
-}, [`detail ${id}`], {revalidate:2})
+const getDetail = unstable_cache(async (id: number | string) => {
+  const response = await ProductService.show(id);
+  return response.context;
+}, ["product-detail"], { revalidate: 2 });
 
+export default async function ProductDetailPage(props: PageProps) {
+  const id = (await props.params).id
+  const detail = await getDetail(1);
+  console.log(id);
+  console.log(detail);
 
-export default async function ProductDetailPage({ params }: { params: { id: string } }){
-  const detail = await getDetail(1)()
-  console.log(params.id)
-  console.log(detail)
   return (
     <div>
-      ProductDetailPage
+      Product Detail Page
     </div>
-  )
+  );
 }
